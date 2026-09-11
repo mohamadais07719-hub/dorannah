288707f9abefa9a5d1fa63ecf8d11bcb74c2e957
mohamadais07719-hub <mohamadais07719@gmail.com>
2026-09-11 07:35:13 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="dorannah-v4";
+const CACHE="dorannah-v5";
 const FILES=["./","./index.html","./doctors.js","./labs.js","./devices.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
